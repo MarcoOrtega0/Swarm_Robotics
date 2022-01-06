14ff9f5acaf622f7ff43499b3851a054be24f861
Marco Ortega <marquitoortega0@gmail.com>
2022-01-05 22:34:29 -0500

    actualizacion

--- Matlab/arrow_draw.m
+++ Matlab/arrow_draw.m
@@ -4,7 +4,7 @@ posy=[0.816*(y+lim)/(2*lim)+0.111,0.816*((y+0.22*sin(yaw))+lim)/(2*lim)+0.111];
 ar=annotation("arrow",posx,posy);
 ar.HeadStyle='cback2';
 ar.Color=color;
-ar.HeadLength=10;
+ar.HeadLength=12;
 ar.HeadWidth=10;
 % ar.LineStyle='none';
 end
