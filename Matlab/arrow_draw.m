6eec4014b4beefd4a51baa4d76a6ad113fdb0f9d
Marco Ortega <marquitoortega0@gmail.com>
2022-01-09 10:20:36 -0500

    fin interfaz final final

--- Matlab/arrow_draw.m
+++ Matlab/arrow_draw.m
@@ -1,5 +1,5 @@
 function ar=arrow_draw(x,y,yaw,lim,color)
-posx=[0.775*(x+lim)/(2*lim)+0.138,0.775*((x+0.22*cos(yaw))+lim)/(2*lim)+0.138];
+posx=[0.775*(x+lim)/(2*lim)+0.13,0.775*((x+0.22*cos(yaw))+lim)/(2*lim)+0.13];
 posy=[0.816*(y+lim)/(2*lim)+0.111,0.816*((y+0.22*sin(yaw))+lim)/(2*lim)+0.111];
 ar=annotation("arrow",posx,posy);
 ar.HeadStyle='cback2';
